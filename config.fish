a00c27cb3b4e37a3c0f043966c4de911418f44b7
1ocate <integrityms@gmail.com>
2022-12-09 01:04:55 +0900

    fix : fish 설정 변경

--- config.fish
+++ config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fzf_configure_bindings --directory=\ct
-alias vi="nvim"
-alias vim="nvim"
+    fzf_configure_bindings --directory=\ct --variables=\ch
+    alias vi="nvim"
+    alias vim="nvim"
+    set fzf_fd_opts --hidden --exclude=.git
 end
+
