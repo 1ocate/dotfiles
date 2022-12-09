faf1bd07d8d50f6d6d4632e47b22f18ed420d18c
1ocate <integrityms@gmail.com>
2022-12-09 11:35:34 +0900

    fix : vi-binding, greethings, fzf binding등 설정 변경

--- config.fish
+++ config.fish
@@ -1,8 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    fzf_configure_bindings --directory=\ct --variables=\ch
+    fzf_configure_bindings --directory=\ct --variables=\ch --git_log=\cg
     alias vi="nvim"
     alias vim="nvim"
     set fzf_fd_opts --hidden --exclude=.git
+    set -U fish_greeting "🐟"
+    fish_vi_key_bindings
 end
 
+function fish_user_key_bindings
+    for mode in insert default visual
+        bind -M $mode \ce forward-char
+    end
+end
