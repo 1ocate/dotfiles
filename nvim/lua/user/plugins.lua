f16dc65a3d559161f9af0db934b4aca859b62d2f
locate <integrityms@gmail.com>
2023-01-15 22:30:16 +0900

    Feature : nvim-lua 마이그레이션

---
+++ nvim/lua/user/plugins.lua
@@ -0,0 +1,251 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+require('packer').reset()
+require('packer').init({
+  compile_path = vim.fn.stdpath('data')..'/site/plugin/packer_compiled.lua',
+ display = {
+   open_fn = function()
+     return require('packer.util').float({ border = 'solid' })
+   end,
+ },
+})
+
+local use = require('packer').use
+
+-- Packer can manage itself.
+use('wbthomason/packer.nvim')
+
+-- Commenting support.
+use('tpope/vim-commentary')
+
+-- Add, change, and delete surrounding text.
+use("tpope/vim-surround")
+
+-- Useful commands like :Rename and :SudoWrite.
+use('tpope/vim-eunuch')
+
+-- Pairs of handy bracket mappings, like [b and ]b.
+use('tpope/vim-unimpaired')
+
+-- Indent autodetection with editorconfig support.
+use('tpope/vim-sleuth')
+
+-- Allow plugins to enable repeating of commands.
+use('tpope/vim-repeat')
+
+-- Add more languages.
+use('sheerun/vim-polyglot')
+
+-- Navigate seamlessly between Vim windows and Tmux panes.
+use('christoomey/vim-tmux-navigator')
+
+-- Jump to the last location when opening a file.
+use('farmergreg/vim-lastplace')
+
+-- Enable * searching with visually selected text.
+use('nelstrom/vim-visual-star-search')
+
+-- Automatically create parent dirs when saving.
+use('jessarcher/vim-heritage')
+
+-- Text objects for HTML attributes.
+use({
+  'whatyouhide/vim-textobj-xmlattr',
+  requires = 'kana/vim-textobj-user',
+})
+
+-- Automatically set the working directory to the project root.
+use({
+  'airblade/vim-rooter',
+  setup = function()
+    -- Instead of this running every time we open a file, we'll just run it once when Vim starts.
+    vim.g.rooter_manual_only = 1
+  end,
+  config = function()
+    vim.cmd('Rooter')
+  end,
+})
+
+-- Automatically add closing brackets, quotes, etc.
+use({
+  'windwp/nvim-autopairs',
+  config = function()
+    require('nvim-autopairs').setup()
+  end,
+})
+
+-- Add smooth scrolling to avoid jarring jumps
+use({
+  'karb94/neoscroll.nvim',
+  config = function()
+    require('neoscroll').setup()
+  end,
+})
+
+-- All closing buffers without closing the split window.
+use({
+  'famiu/bufdelete.nvim',
+  config = function()
+    vim.keymap.set('n', '<Leader>q', ':Bdelete<CR>')
+  end,
+})
+
+-- Split arrays and methods onto multiple lines, or join them back up.
+use({
+  'AndrewRadev/splitjoin.vim',
+  config = function()
+    vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
+    vim.g.splitjoin_trailing_comma = 1
+    vim.g.splitjoin_php_method_chain_full = 1
+  end,
+})
+
+-- Automatically fix indentation when pasting code.
+use({
+  'sickill/vim-pasta',
+  config = function()
+    vim.g.pasta_disabled_filetypes = { 'fugitive' }
+  end,
+})
+
+-- Clipboard system sync only yank.
+use({
+  '1ocate/yankclip.vim',
+  config = function()
+    vim.g.os_clipboard_enble = 1
+    vim.keymap.set('n', '<F9>', ':OsYankToggle <CR>')
+  end,
+})
+
+-- gruvbox theme.
+use({
+    'morhetz/gruvbox',
+  config = function()
+    vim.g.gruvbox_contrast_dark = 'hard'
+    vim.g.gruvbox_italic = 1
+    vim.opt.background = 'dark'
+    vim.cmd('colorscheme gruvbox')
+
+    vim.api.nvim_set_hl(0, 'FloatBorder', {
+      fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+      bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+    })
+  end,
+
+})
+
+-- Vim bible
+use({
+  'robertrosman/vim-bible',
+  config = function()
+    vim.g.BibleTranslation = 'KorRv'
+    vim.g.BibleLocale = 'en'
+    -- vim.keymap.set('v', '<Leader>y', ':call bible#insert_quote(@")<CR>')
+    -- vim.keymap.set('v', '<Leader>y', ':d <bar> :call bible#insert_quote(substitute(@", "\n$", "", ""))<CR>')
+    -- vim.keymap.set('v', '<Leader>y', ':d <bar> :call bible#insert_quote(@")<CR>')
+    -- vnoremap <Leader>y :call bible#insert_quote(substitute("창1:1", '\n$', '', ''))<CR>
+    -- vnoremap <Leader>y :call bible#insert_quote("창10:1")<CR>
+    vim.cmd([[
+      noremap <Leader>b y :d_ <bar> :call bible#insert_quote(substitute(@", '\n$', '', ''))<CR>
+      "vnoremap <Leader>b y :d <bar> :call bible#insert_quote(substitute(@", '\n$', '', ''))<CR>
+    ]])
+  end,
+})
+
+-- Fuzzy finder
+use({
+  'nvim-telescope/telescope.nvim',
+  requires = {
+    'nvim-lua/plenary.nvim',
+    'kyazdani42/nvim-web-devicons',
+    'nvim-telescope/telescope-live-grep-args.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+  },
+  config = function()
+    require('user/plugins/telescope')
+  end,
+})
+
+-- File tree sidebar
+use({
+  'kyazdani42/nvim-tree.lua',
+  requires = 'kyazdani42/nvim-web-devicons',
+  config = function()
+    require('user/plugins/nvim-tree')
+  end,
+})
+
+-- Vim Fugitive
+use({
+  'tpope/vim-fugitive',
+  requires = 'tpope/vim-rhubarb',
+  cmd = 'G',
+})
+
+-- Vim Test
+use({
+  'vim-test/vim-test',
+  config = function()
+    require('user.plugins.vim-test')
+  end,
+})
+
+-- nvim Lsp
+use({
+  'neovim/nvim-lspconfig',
+  requires = {
+    'b0o/schemastore.nvim',
+    'folke/lsp-colors.nvim',
+  },
+  config = function()
+    require('user.plugins.lspconfig')
+  end,
+})
+
+use({
+  'L3MON4D3/LuaSnip',
+  config = function()
+    require('user.plugins.luasnip')
+  end,
+})
+
+use({
+  'hrsh7th/nvim-cmp',
+  requires = {
+    'L3MON4D3/LuaSnip',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-nvim-lsp-signature-help',
+    'hrsh7th/cmp-nvim-lua',
+    'jessarcher/cmp-path',
+    'onsails/lspkind-nvim',
+    'saadparwaiz1/cmp_luasnip',
+  },
+  config = function()
+    require('user.plugins.cmp')
+  end,
+})
+
+-- Put this at the end after all plugins
+if packer_bootstrap then
+    require('packer').sync()
+end
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile>
+  augroup end
+]])
