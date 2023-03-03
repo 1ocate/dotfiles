b0b3081ecf1991126f7b0a4253bb1ddda0c11b1a
1ocate <integrityms@gmail.com>
2023-03-03 13:43:35 +0900

    cargo 경로추가, apt-get alias 변경

--- config.fish
+++ config.fish
@@ -2,11 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     alias vi='nvim'
     alias vim='nvim'
+    alias apt-get='sudo apt-get'
     fzf_configure_bindings --directory=\ct --variables=\ch --git_log=\cg
     set fzf_fd_opts --hidden --exclude=.git
     set -U fish_greeting "🐟"
     fish_vi_key_bindings
-    set -x PATH $PATH ~/bin ~/.local/bin/ /opt/homebrew/bin /opt/local/bin /opt/local/sbin
+    set -x PATH $PATH ~/bin ~/.local/bin/ /opt/homebrew/bin /opt/local/bin /opt/local/sbin ~/.cargo/bin
 end
 
 function fish_user_key_bindings
