5ef40e2fd72aa9c7cdc03bede1c60902471d2898
1ocate <integrityms@gmail.com>
2023-05-12 10:44:47 +0900

    속도 이슈로 비활성화

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,5 +1,5 @@
-vim.opt.cursorline = true
-vim.opt.cursorcolumn = true
+-- vim.opt.cursorline = true
+-- vim.opt.cursorcolumn = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
