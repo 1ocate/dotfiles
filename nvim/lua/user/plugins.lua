bcff0332743c23291ba876f8ba614881a3219e97
locate <integrityms@gmail.com>
2024-09-07 00:06:49 +0900

    feat: Copilot 플러그인 설정 개선
    - Copilot 플러그인에 대한 상세 설정 추가
    - 특정 키 매핑 및 레이아웃으로 패널 활성화
    - 제안 설정에 키 매핑 구성
    - 특정 파일 타입에 대해 Copilot 비활성화
    - Copilot에 대한 Node.js 명령어 설정
    - CopilotChat 디버그 설정을 false로 업데이트

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -443,7 +443,50 @@ use {
   cmd = "Copilot",
   event = "InsertEnter",
   config = function()
-    require("copilot").setup({})
+    require('copilot').setup({
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<C-j>"
+        },
+        layout = {
+          position = "bottom", -- | top | left | right
+          ratio = 0.4
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = false,
+        hide_during_completion = true,
+        debounce = 75,
+        keymap = {
+          accept = "<M-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
+      copilot_node_command = 'node', -- Node.js version must be > 18.x
+      server_opts_overrides = {},
+    })
   end,
 }
 
@@ -457,7 +500,7 @@ use {
   run = "make tiktoken",  -- Only on MacOS or Linux
   config = function()
     require("CopilotChat").setup {
-      debug = true,  -- Enable debugging
+      debug = false,  -- Enable debugging
     }
   end
 }
