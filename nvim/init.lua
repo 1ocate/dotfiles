d81d3ce05136050b37cd3e56da3da21dec5e1966
locate <integrityms@gmail.com>
2024-09-17 10:19:11 +0900

    feat: lazyvim 추가

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,2 @@
--- OS Check
-local homePwd = os.getenv('HOME')
-local wslEnv = os.getenv('WSLENV')
-
-osName =''
-if homePwd then
-    if string.match(homePwd, "/Users") then
-        osName = 'Mac'
-    else
-        osName = 'Linux'
-
-        if wslEnv then
-            osName = 'WSL'
-        end
-    end
-else
-    osName ='Other'
-end
-
-autocomplete = 'cmp'
-visualSelectMode = true
-
-
-require('user.options')
-require('user.plugins')
-require('user.keymaps')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
