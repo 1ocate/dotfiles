b79beedf156bed8bd4ea017131fd8418a95aff76
locate <integrityms@gmail.com>
2024-09-21 22:33:44 +0900

    feat: OS 감지 및 자동 완성 설정 추가
    - 사용자 홈 디렉토리 경로를 통해 OS 감지 로직 추가
    - Mac, Linux, WSL, 기타 OS 구분
    - 자동 완성 설정 변수 추가 (autocomplete)
    - 시각적 선택 모드 설정 변수 추가 (visualSelectMode)

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,22 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+local homePwd = os.getenv("HOME")
+local wslEnv = os.getenv("WSLENV")
+
+osName = ""
+if homePwd then
+  if string.match(homePwd, "/Users") then
+    osName = "Mac"
+  else
+    osName = "Linux"
+
+    if wslEnv then
+      osName = "WSL"
+    end
+  end
+else
+  osName = "Other"
+end
+
+autocomplete = "cmp"
+visualSelectMode = true
 require("config.lazy")
