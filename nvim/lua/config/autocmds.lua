2cdfce5372f22714e51bb42c7cc67739c898240d
locate <integrityms@gmail.com>
2024-09-21 22:44:07 +0900

    fix: gitcommit 파일 타입 처리 로직 수정
    디버깅 출력 및 불필요한 파일 저장 명령어 제거
    - gitcommit 파일 타입 확인 후 커밋 메시지 파일로 커밋 실행
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch lazyvim
    # Your branch is ahead of 'origin/lazyvim' by 6 commits.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	modified:   nvim/lua/config/autocmds.lua
    #
    # Untracked files:
    #	node_modules/
    #	nvim_bak/
    #	package-lock.json
    #	package.json
    #	scripts/.project_path.example
    #

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -16,8 +16,6 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "*.git/COMMIT_EDITMSG",
   callback = function()
     if vim.bo.filetype == "gitcommit" then
-      print("Autocmd triggered for gitcommit") -- 디버깅을 위한 출력
-      vim.cmd("w") -- 파일 저장
       vim.cmd("Git commit -F %") -- 커밋 메시지 파일을 사용하여 커밋
     end
   end,
