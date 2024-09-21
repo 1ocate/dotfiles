3c17cc1146f1ffabb8b96208e4aa9cc937ad518f
locate <integrityms@gmail.com>
2024-09-21 22:41:45 +0900

    feat: 자동 커밋 메시지 처리 추가
    자동 커밋 메시지 처리를 위한 autocmd 추가
    - BufRead 이벤트에서 gitcommit 파일 타입 확인
    - BufWritePost 이벤트에서 커밋 메시지 파일 저장 및 커밋 실행
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch lazyvim
    # Your branch is ahead of 'origin/lazyvim' by 4 commits.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	modified:   nvim/lua/config/autocmds.lua
    #
    # Changes not staged for commit:
    #	modified:   nvim/lua/plugins/which-key.lua
    #
    # Untracked files:
    #	node_modules/
    #	nvim_bak/
    #	package-lock.json
    #	package.json
    #	scripts/.project_path.example
    #

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,24 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+vim.api.nvim_create_autocmd("BufRead", {
+  pattern = "*.git/COMMIT_EDITMSG",
+  callback = function()
+    if vim.bo.filetype == "gitcommit" then
+      print("Autocmd triggered for gitcommit") -- 디버깅을 위한 출력
+      vim.cmd("CopilotChatCommitStage")
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.git/COMMIT_EDITMSG",
+  callback = function()
+    if vim.bo.filetype == "gitcommit" then
+      print("Autocmd triggered for gitcommit") -- 디버깅을 위한 출력
+      vim.cmd("w") -- 파일 저장
+      vim.cmd("Git commit -F %") -- 커밋 메시지 파일을 사용하여 커밋
+    end
+  end,
+})
