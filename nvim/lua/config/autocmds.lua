bcd0820371b34b5a4090be9054312ec9e086f8de
locate <integrityms@gmail.com>
2024-09-22 20:00:37 +0900

    feat(autocmds): COMMIT_EDITMSG 파일에 대한 BufReadPost 자동 명령 추가
    BufReadPost 이벤트에 대해 COMMIT_EDITMSG 파일을 처리하는 자동 명령을 추가했습니다.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,7 +6,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 --
---
 vim.api.nvim_create_autocmd("BufReadPost", {
   pattern = "*.git/COMMIT_EDITMSG",
   callback = function()
