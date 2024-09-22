d94d0ab90997d07f836383af2aa7d4b4d37c345e
locate <integrityms@gmail.com>
2024-09-22 19:38:35 +0900

    feat: 자동 명령어 수정 및 디버깅 코드 추가
    - BufReadPost 이벤트로 자동 명령어 변경
    - gitcommit 파일 타입 확인 후 CopilotChatCommitStage 명령어 실행
    - 디버깅을 위한 출력 코드 추가

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,35 +6,44 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 --
-vim.api.nvim_create_autocmd("BufRead", {
+--
+vim.api.nvim_create_autocmd("BufReadPost", {
   pattern = "*.git/COMMIT_EDITMSG",
   callback = function()
     if vim.bo.filetype == "gitcommit" then
-      print("Autocmd triggered for gitcommit") -- 디버깅을 위한 출력
       vim.cmd("CopilotChatCommitStage")
     end
   end,
 })
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = "*.git/COMMIT_EDITMSG",
-  callback = function()
-    if vim.bo.filetype == "gitcommit" then
-      local lines = vim.fn.getline(1, "$") -- 모든 줄 가져오기
-      local commit_message = ""
-      for _, line in ipairs(lines) do
-        if not vim.startswith(line, "#") then
-          commit_message = commit_message .. line .. "\n"
-        end
-      end
-      commit_message = vim.trim(commit_message) -- 앞뒤 공백 제거
-
-      if commit_message ~= "" then -- 커밋 메시지가 비어 있지 않은 경우에만
-        local temp_file = vim.fn.tempname()
-        vim.fn.writefile(vim.split(commit_message, "\n"), temp_file)
-        vim.cmd("Git commit -F " .. temp_file) -- 임시 파일을 사용하여 커밋
-        vim.fn.delete(temp_file) -- 임시 파일 삭제
-      end
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufRead", {
+--   pattern = "*.git/COMMIT_EDITMSG",
+--   callback = function()
+--     if vim.bo.filetype == "gitcommit" then
+--       print("Autocmd triggered for gitcommit") -- 디버깅을 위한 출력
+--       vim.cmd("CopilotChatCommitStage")
+--     end
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--   pattern = "*.git/COMMIT_EDITMSG",
+--   callback = function()
+--     if vim.bo.filetype == "gitcommit" then
+--       local lines = vim.fn.getline(1, "$") -- 모든 줄 가져오기
+--       local commit_message = ""
+--       for _, line in ipairs(lines) do
+--         if not vim.startswith(line, "#") then
+--           commit_message = commit_message .. line .. "\n"
+--         end
+--       end
+--       commit_message = vim.trim(commit_message) -- 앞뒤 공백 제거
+--
+--       if commit_message ~= "" then -- 커밋 메시지가 비어 있지 않은 경우에만
+--         local temp_file = vim.fn.tempname()
+--         vim.fn.writefile(vim.split(commit_message, "\n"), temp_file)
+--         vim.cmd("Git commit -F " .. temp_file) -- 임시 파일을 사용하여 커밋
+--         vim.fn.delete(temp_file) -- 임시 파일 삭제
+--       end
+--     end
+--   end,
+-- })
