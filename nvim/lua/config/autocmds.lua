e723d9348e5a787c3606bc000d40f747aa57f804
locate <integrityms@gmail.com>
2024-09-22 10:57:12 +0900

    feat: 커밋 메시지 유효성 검사 및 자동 버퍼 닫기 기능 추가
    - 첫 번째 줄의 커밋 메시지가 비어 있지 않은 경우에만 커밋
    - 현재 버퍼가 fugitive 버퍼가 아닌 경우에만 닫기
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch lazyvim
    # Your branch is up to date with 'origin/lazyvim'.
    #
    # Changes to be committed:
    #	modified:   nvim/lua/config/autocmds.lua
    #
    # Untracked files:
    #	nvim_bak/
    #

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -16,14 +16,17 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "*.git/COMMIT_EDITMSG",
   callback = function()
     if vim.bo.filetype == "gitcommit" then
-      vim.cmd("w") -- 파일 저장
-      vim.cmd("Git commit -F %") -- 커밋 메시지 파일을 사용하여 커밋
-      local bufnr = vim.fn.bufnr("%") -- 현재 버퍼 번호 가져오기
-      local bufname = vim.fn.bufname(bufnr) -- 현재 버퍼 이름 가져오기
+      local commit_message = vim.fn.getline(1) -- 첫 번째 줄의 커밋 메시지 가져오기
+      if commit_message ~= "" then -- 커밋 메시지가 비어 있지 않은 경우에만
+        vim.cmd("w") -- 파일 저장
+        vim.cmd("Git commit -F %") -- 커밋 메시지 파일을 사용하여 커밋
+        local bufnr = vim.fn.bufnr("%") -- 현재 버퍼 번호 가져오기
+        local bufname = vim.fn.bufname(bufnr) -- 현재 버퍼 이름 가져오기
 
-      -- 현재 버퍼가 fugitive 버퍼가 아닌 경우에만 닫기
-      if not string.match(bufname, "^fugitive://") then
-        vim.cmd("bwipeout " .. bufnr) -- 현재 버퍼 제거
+        -- 현재 버퍼가 fugitive 버퍼가 아닌 경우에만 닫기
+        if not string.match(bufname, "^fugitive://") then
+          vim.cmd("bwipeout " .. bufnr) -- 현재 버퍼 제거
+        end
       end
     end
   end,
