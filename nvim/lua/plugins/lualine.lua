eed9afea0dda63499e7820a1c38b98b40a08074f
locate <integrityms@gmail.com>
2024-09-22 09:39:55 +0900

    feat: lualine 설정 파일 추가
    nvim-lualine 플러그인의 설정 파일을 추가했습니다. 이 설정 파일은
    상태 라인에 표시될 구성 요소와 테마를 정의합니다.
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch lazyvim
    # Your branch is ahead of 'origin/lazyvim' by 3 commits.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	new file:   nvim/lua/plugins/lualine.lua
    #
    # Untracked files:
    #	nvim_bak/
    #

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,49 @@
+local separator = {
+  '"▏"',
+  color = "StatusLineNonText",
+}
+
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        globalstatus = false,
+        theme = "auto",
+      },
+      sections = {
+        lualine_a = {
+          "mode",
+          -- separator,
+        },
+        lualine_b = {
+          "branch",
+          "diff",
+          -- separator,
+          -- '"🖧  " .. tostring(#vim.tbl_keys(vim.lsp.buf_get_clients()))',
+          -- { 'diagnostics', sources = { 'nvim_diagnostic' } },
+          -- separator,
+        },
+        lualine_c = {
+          "filename",
+        },
+        lualine_x = {
+          "filetype",
+          "encoding",
+          "fileformat",
+        },
+        lualine_y = {
+          -- separator,
+          '(vim.bo.expandtab and "SP" or "Tab") .. " " .. vim.bo.shiftwidth',
+          -- separator,
+        },
+        lualine_z = {
+          "location",
+          "progress",
+        },
+      },
+    },
+  },
+}
