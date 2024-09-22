ed2b0f2c75cb12488e79f59b45b5fb618405d545
locate <integrityms@gmail.com>
2024-09-22 20:17:21 +0900

    fix: 중복된 기본 자동 명령 제거
    - 기본 자동 명령이 중복되어 있는 부분을 제거했습니다.
    - 코드의 가독성과 유지보수성을 향상시켰습니다.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,10 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 --
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
---
 vim.api.nvim_create_autocmd("BufRead", {
   pattern = "*.git/COMMIT_EDITMSG",
   callback = function()
