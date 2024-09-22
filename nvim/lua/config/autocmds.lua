b3af683eecb6149f99856ee69d0525a96282eb0f
locate <integrityms@gmail.com>
2024-09-22 20:10:46 +0900

    fix: 자동 명령어 버그 수정 및 성능 개선
    - gitcommit 파일 타입에서 주석을 제외한 커밋 메시지 생성 로직 수정
    - 임시 파일을 사용하여 커밋 후 버퍼를 닫는 로직 개선
    - fugitive 버퍼가 아닌 경우에만 버퍼를 닫도록 조건 추가

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -21,13 +21,12 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   callback = function()
     if vim.bo.filetype == "gitcommit" then
       local lines = vim.fn.getline(1, "$") -- 모든 줄 가져오기
-      local commit_message = table.concat(
-        vim.tbl_filter(function(line)
-          return not vim.startswith(line, "#")
-        end, lines),
-        "\n"
-      )
-
+      local commit_message = ""
+      for _, line in ipairs(lines) do
+        if not vim.startswith(line, "#") then
+          commit_message = commit_message .. line .. "\n"
+        end
+      end
       commit_message = vim.trim(commit_message) -- 앞뒤 공백 제거
 
       if commit_message ~= "" then -- 커밋 메시지가 비어 있지 않은 경우에만
@@ -36,10 +35,13 @@ vim.api.nvim_create_autocmd("BufWritePost", {
         vim.cmd("Git commit -F " .. temp_file) -- 임시 파일을 사용하여 커밋
         vim.fn.delete(temp_file) -- 임시 파일 삭제
 
-        vim.cmd("Git")
+        local bufnr = vim.fn.bufnr("%") -- 현재 버퍼 번호 가져오기
+        local bufname = vim.fn.bufname(bufnr) -- 현재 버퍼 이름 가져오기
 
-        -- 버퍼 지우기
-        vim.cmd("bwipeout")
+        -- 현재 버퍼가 fugitive 버퍼가 아닌 경우에만 닫기
+        if not string.match(bufname, "^fugitive:///") then
+          vim.cmd("bwipeout " .. bufnr) -- 현재 버퍼 제거
+        end
       end
     end
   end,
