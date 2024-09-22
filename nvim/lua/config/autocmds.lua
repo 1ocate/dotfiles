d5b559f0d5fa55df0cc669bdc3c019e8b5642cfa
locate <integrityms@gmail.com>
2024-09-22 10:23:56 +0900

    fix: fugitive 버퍼가 닫히지 않도록 수정
    - COMMIT_EDITMSG 버퍼 저장 후 fugitive 버퍼가 닫히지 않도록 조건 추가
    - 현재 버퍼가 fugitive 버퍼가 아닌 경우에만 bwipeout 실행
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch lazyvim
    # Your branch is up to date with 'origin/lazyvim'.
    #
    # Changes to be committed:
    #	modified:   nvim/lua/config/autocmds.lua
    #
    # Untracked files:
    #	nvim_bak/
    #

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -19,7 +19,12 @@ vim.api.nvim_create_autocmd("BufWritePost", {
       vim.cmd("w") -- 파일 저장
       vim.cmd("Git commit -F %") -- 커밋 메시지 파일을 사용하여 커밋
       local bufnr = vim.fn.bufnr("%") -- 현재 버퍼 번호 가져오기
-      vim.cmd("bwipeout " .. bufnr) -- 현재 버퍼 제거
+      local bufname = vim.fn.bufname(bufnr) -- 현재 버퍼 이름 가져오기
+
+      -- 현재 버퍼가 fugitive 버퍼가 아닌 경우에만 닫기
+      if not string.match(bufname, "^fugitive://") then
+        vim.cmd("bwipeout " .. bufnr) -- 현재 버퍼 제거
+      end
     end
   end,
 })
